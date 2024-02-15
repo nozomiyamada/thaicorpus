936baa53d3bedd4f734d282b7f21514c347c4dba
Nozomi Yamada <44984892+nozomiyamada@users.noreply.github.com>
2024-02-15 20:37:12 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-cp words_th.txt .venv/lib/python3.7/site-packages/pythainlp/corpus/words_th.txt
-source .venv/bin/activate
+cp words_th.txt venv/lib/python3.9/site-packages/pythainlp/corpus/words_th.txt
+source venv/bin/activate
 gunicorn --daemon app:app --bind=0.0.0.0:8000
